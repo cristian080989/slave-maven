62ebf29029c3719f8aea3b91393328231684aec3
cristian080989 <cristian080989@gmail.com>
2022-01-12 18:54:41 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,9 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+
+
+# Install de aws cli
+sudo yum -y update     
+sudo yum -y install aws-cli 
+aws --version   
